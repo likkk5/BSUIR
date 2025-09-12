7f0ee9b2433407789f43a02b29226f0ee851361e
lishykksu <lishykksu@gmail.com>
2025-09-13 02:49:34 +0300

    add 1course/bsuir open

---
+++ 1 курс/bsuir open/B/B.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double n, m, k;
+    cin >> n >> m >> k;
+    double left = 0;
+    double right = min(n, m);
+    double epsilon = 0.000001;
+    double ans = 0;
+
+    while (right - left > epsilon) {
+        double mid = (left + right) / 2;
+        double squares = floor(n / mid) * floor(m / mid);
+
+        if (squares >= k) {
+            ans = mid;
+            left = mid;
+        }
+        else {
+            right = mid;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
+
