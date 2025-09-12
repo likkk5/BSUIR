7908286961c7c635fd96b542f8ffe3145a6b398b
lishykksu <lishykksu@gmail.com>
2025-09-13 02:23:44 +0300

    add bsuir open

---
+++ 1 курс/bsuir open/J/J.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+long long f(long long a, long long b, long long x)
+{
+    if (x <= a)
+    {
+        x = a + 1;
+    }
+    return x - b + 1;
+  
+}
+
+int main()
+{
+    int t;
+
+    long long a, b, x;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        std::cin >> a >> b >> x;
+        std::cout << f(a, b, x) << std::endl;
+    }
+    return 0;
+}
