0ab2dd9ac30913ffde2e156f456d860f4c367ff8
lishykksu <lishykksu@gmail.com>
2025-09-13 02:31:44 +0300

    Remove ignored files from repo

--- 1 курс/1сем/оаип/repos/exam/350/350.cpp
+++
@@ -1,9 +0,0 @@
-﻿//Найти сколько раз в текстовом файле встречается k - ое слово. (k - вводится с клавиатуры, доп.массивов не использовать).
-#include <iostream>
-#include <fstream>
-int main()
-{
-	std::ifstream Voc;
-	Voc.open("voc.txt");
-}
-
--- 1 курс/1сем/оаип/repos/staticlib/Header.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-int InputArraySize(std::string);
-double InputElementArray(int, int, int, int);
-double** InputArray(double**, int, int);
-void OutputArray(double**, int, int);
-double** CreateNewArray(double**, double**, double**, int, int);
