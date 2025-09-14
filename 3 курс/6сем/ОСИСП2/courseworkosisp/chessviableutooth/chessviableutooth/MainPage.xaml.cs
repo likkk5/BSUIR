003414e7482a6ab66237a85d0bef528e62df56df
lishykksu <lishykksu@gmail.com>
2025-09-14 16:03:14 +0300

    add 3 курс/6сем/ОСИСП2

---
+++ 3 курс/6сем/ОСИСП2/courseworkosisp/chessviableutooth/chessviableutooth/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace chessviableutooth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 3 курс/6сем/ОСИСП2/courseworkosisp/chessviableutooth/chessviableutooth/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE;
+using chessviableutooth.Services;
+using System.Reflection;
+
+namespace chessviableutooth
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton<IBluetoothLE>(CrossBluetoothLE.Current);
+            builder.Services.AddSingleton<IAdapter>(CrossBluetoothLE.Current.Adapter);
+            builder.Services.AddSingleton<ChessGameService>();
+
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+            string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            Console.WriteLine($"🚀 Имя C#-сборки: {assemblyName}");
+            return builder.Build();
+        }
+    }
+}
